792d29cab9c172ac7a9736a5019f3d2185c3c241
Kilian Steenman <37879201+KilianSteenman@users.noreply.github.com>
2018-04-22 10:26:54 +0200

    Implement OTD export

--- src/shadowlink_iv_exporter/textures/texture_exporter.rb
+++ src/shadowlink_iv_exporter/textures/texture_exporter.rb
@@ -4,19 +4,33 @@ def export_entities_textures(entities, export_path)
   end
 end
 
+def export_open_formats(file, dictionary_name, materials)
+  file.puts "Version 8 10"
+  file.puts "{"
+  materials.each do |material|
+    file.puts "\tgrcTexture"
+    file.puts "\t{"
+    file.puts "\t\tType regular"
+    file.puts "\t\tName #{dictionary_name}\\#{material.texture.filename}"
+    file.puts "\t\tMipMaps 1" #TODO: Calculate mipmaps
+    file.puts "\t\tPixelFormat DXT1" #TODO: Define PixelFormat
+    file.puts "\t}"
+  end
+  file.puts "}"
+end
+
 def export_entity_textures(ent, export_path)
   materials = get_materials_for_entity(ent)
-  ide_name = ent.definition.name #definition.get_attribute 'sl_iv_ide', 'ideName'
-  wtd_name = ent.definition.name #definition.get_attribute 'sl_iv_ide', 'wtdName'
-  img_name = "#{ide_name}_img"
+  dictionary_name = ent.definition.name #definition.get_attribute 'sl_iv_ide', 'wtdName'
+  dictionary_path = "#{export_path}/#{dictionary_name}"
+  otd_file = "#{export_path}/#{dictionary_name}.otd"
 
-  image_path = "#{export_path}/#{img_name}/"
-  createDir(image_path)
-  wtd_path =  "#{export_path}/#{img_name}/#{wtd_name}/"
-  createDir(wtd_path)
+  File.open(otd_file, 'w') do |file|
+    export_open_formats(file, dictionary_name, materials)
+  end
 
+  createDir(dictionary_path)
   tw = Sketchup.create_texture_writer
-
   materials.each do |mat|
     material_done = false
     faces = ent.definition.entities.find_all {|e| e.typename == "Face"} # Get all face enteties
@@ -24,7 +38,7 @@ def export_entity_textures(ent, export_path)
       if material_done == false
         if face.material == mat
           tw.load face, true
-          return_val = tw.write(face, true, wtd_path + stripTexName(face.material.texture.filename) + ".png")
+          return_val = tw.write(face, true, "#{dictionary_path}/#{stripTexName(face.material.texture.filename)}.png")
           material_done = true
         end
       end
