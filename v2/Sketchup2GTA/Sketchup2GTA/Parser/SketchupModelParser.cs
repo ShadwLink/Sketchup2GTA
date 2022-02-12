d3a6ae8692395ce92a40ec19c3dd53e6ad039d79
Shadow-Link <37879201+KilianSteenman@users.noreply.github.com>
2022-02-12 19:53:30 +0100

    Extract sketchup model parsing to methods

--- v2/Sketchup2GTA/Sketchup2GTA/Parser/SketchupModelParser.cs
+++ v2/Sketchup2GTA/Sketchup2GTA/Parser/SketchupModelParser.cs
@@ -17,74 +17,80 @@ namespace Sketchup2GTA.Parser
             SketchUp skp = new SketchUp();
             if (skp.LoadModel(path, true))
             {
-                var meshesByMaterial = new Dictionary<Material, List<Mesh>>();
-                foreach (var surface in skp.Surfaces)
-                {
-                    if (surface.FrontMaterial.Name == "")
-                    {
-                        Console.WriteLine("Sad days, empty materials are not supported (yet).");
-                    }
-                    else
-                    {
-                        if (!meshesByMaterial.ContainsKey(surface.FrontMaterial))
-                        {
-                            meshesByMaterial.Add(surface.FrontMaterial, new List<Mesh>());
-                        }
-
-                        meshesByMaterial[surface.FrontMaterial].Add(surface.FaceMesh);
-                    }
-                }
-
-                Model model = new Model(modelName);
-
-                Console.WriteLine("Material Count: " + meshesByMaterial.Count);
-                foreach (var meshByMaterial in meshesByMaterial)
-                {
-                    var material = meshByMaterial.Key;
-                    MaterialSplit split = new MaterialSplit(
-                        new Data.Model.Material(
-                            material.MaterialTexture.Name,
-                            new MaterialColor(
-                                material.Colour.R,
-                                material.Colour.G,
-                                material.Colour.B,
-                                material.Colour.A
-                            )
-                        )
-                    );
-
-                    foreach (var mesh in meshByMaterial.Value)
-                    {
-                        int[] vertexIndexMapping = new int[mesh.Vertices.Count];
-                        for (int i = 0; i < mesh.Vertices.Count; i++)
-                        {
-                            var vertex = mesh.Vertices[i];
-                            vertexIndexMapping[i] = split.AddVertex(new Vector3(
-                                    (float)vertex.X,
-                                    (float)vertex.Y,
-                                    (float)vertex.Z
-                                )
-                            );
-                        }
-
-                        foreach (var face in mesh.Faces)
-                        {
-                            split.AddFaceIndex(vertexIndexMapping[face.A]);
-                            split.AddFaceIndex(vertexIndexMapping[face.C]);
-                            split.AddFaceIndex(vertexIndexMapping[face.B]);
-                        }
-                    }
-
-                    model.AddMaterialSplit(split);
-
-                    Console.WriteLine("Material " + meshByMaterial.Key.MaterialTexture.Name + " vertexCount " +
-                                      split.Vertices.Count + " faceIndexCount " + split.Indices.Count);
-                }
-
-                return model;
+                var meshesByMaterial = GroupMeshDataByMaterial(skp);
+                return CreateModel(modelName, meshesByMaterial);
             }
 
             return null;
+        }
+
+        private Dictionary<Material, List<Mesh>> GroupMeshDataByMaterial(SketchUp skp)
+        {
+            var meshesByMaterial = new Dictionary<Material, List<Mesh>>();
+            foreach (var surface in skp.Surfaces)
+            {
+                if (surface.FrontMaterial.Name == "")
+                {
+                    Console.WriteLine("Sad days, empty materials are not supported (yet).");
+                }
+                else
+                {
+                    if (!meshesByMaterial.ContainsKey(surface.FrontMaterial))
+                    {
+                        meshesByMaterial.Add(surface.FrontMaterial, new List<Mesh>());
+                    }
+
+                    meshesByMaterial[surface.FrontMaterial].Add(surface.FaceMesh);
+                }
+            }
+
+            return meshesByMaterial;
+        }
+
+        private Model CreateModel(string modelName, Dictionary<Material, List<Mesh>> meshesByMaterial)
+        {
+            Model model = new Model(modelName);
+            foreach (var meshByMaterial in meshesByMaterial)
+            {
+                var material = meshByMaterial.Key;
+                MaterialSplit split = new MaterialSplit(
+                    new Data.Model.Material(
+                        material.MaterialTexture.Name,
+                        new MaterialColor(
+                            material.Colour.R,
+                            material.Colour.G,
+                            material.Colour.B,
+                            material.Colour.A
+                        )
+                    )
+                );
+
+                foreach (var mesh in meshByMaterial.Value)
+                {
+                    int[] vertexIndexMapping = new int[mesh.Vertices.Count];
+                    for (int i = 0; i < mesh.Vertices.Count; i++)
+                    {
+                        var vertex = mesh.Vertices[i];
+                        vertexIndexMapping[i] = split.AddVertex(new Vector3(
+                                (float)vertex.X,
+                                (float)vertex.Y,
+                                (float)vertex.Z
+                            )
+                        );
+                    }
+
+                    foreach (var face in mesh.Faces)
+                    {
+                        split.AddFaceIndex(vertexIndexMapping[face.A]);
+                        split.AddFaceIndex(vertexIndexMapping[face.C]);
+                        split.AddFaceIndex(vertexIndexMapping[face.B]);
+                    }
+                }
+
+                model.AddMaterialSplit(split);
+            }
+
+            return model;
         }
     }
 }
