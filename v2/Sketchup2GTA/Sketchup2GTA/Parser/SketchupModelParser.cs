ee5327fc17653ce1d180f447bc1d3a9def797387
Shadow-Link <37879201+KilianSteenman@users.noreply.github.com>
2022-02-21 20:07:39 +0100

    Remove unused txd exporting from model parser

--- v2/Sketchup2GTA/Sketchup2GTA/Parser/SketchupModelParser.cs
+++ v2/Sketchup2GTA/Sketchup2GTA/Parser/SketchupModelParser.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.IO;
 using System.Numerics;
 using Sketchup2GTA.Data.Model;
-using Sketchup2GTA.Exporters.Model.RW;
 using SketchUpNET;
 using Material = SketchUpNET.Material;
 
@@ -19,19 +18,6 @@ namespace Sketchup2GTA.Parser
             if (skp.LoadModel(path, true))
             {
                 var meshesByMaterial = GroupMeshDataByMaterial(skp);
-                
-                // TODO: Move this
-                List<Texture> textures = new List<Texture>();
-                foreach (var item in meshesByMaterial)
-                {
-                    var material = item.Key;
-                    if (material.UsesTexture)
-                    {
-                        textures.Add(material.MaterialTexture);
-                    }
-                }
-                ExportTextures(modelName, textures);
-
                 return CreateModel(modelName, meshesByMaterial);
             }
 
@@ -110,41 +96,6 @@ namespace Sketchup2GTA.Parser
             }
 
             return model;
-        }
-
-        private void ExportTextures(String txdName, List<Texture> textures)
-        {
-            // Bmp
-            Console.WriteLine("Exporting textures " + textures.Count);
-            // var bw = new BinaryWriter(new FileStream(texture.Name, FileMode.OpenOrCreate));
-            // bw.Write((byte)0x42);
-            // bw.Write((byte)0x4D);
-            // bw.Write(texture.Data.Length + 40 + 14);
-            // bw.Write(0);
-            // bw.Write(54); // Data offset
-            // bw.Write(40); // Header size
-            // bw.Write(texture.Width);
-            // bw.Write(texture.Height);
-            // bw.Write((short)1); // Color planes
-            // bw.Write((short)24);
-            // bw.Write(0); // Compression None
-            // bw.Write(texture.Data.Length);
-            // bw.Write(0); // Horizontal res
-            // bw.Write(0); // Vertical res
-            // bw.Write(0); // Colors in palette
-            // bw.Write(0); // Important colors
-            // bw.Write(texture.Data);
-            // bw.Flush();
-            // bw.Close();
-
-            // Txd
-            var bwTxd = new BinaryWriter(new FileStream(txdName + ".txd", FileMode.OpenOrCreate));
-
-            new RwTextureDictionary(textures)
-                .Write(bwTxd, RwVersion.ViceCity);
-
-            bwTxd.Flush();
-            bwTxd.Dispose();
         }
     }
 }
